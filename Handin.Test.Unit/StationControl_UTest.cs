44b943f59a5faa2272f023dd3434caa776e4ed1d
kristianbork <gaardstedkristian@gmail.com>
2021-03-30 12:44:12 +0200

    Started StationControl tests

---
+++ Handin.Test.Unit/StationControl_UTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Classes;
+using Classes.EventArg;
+using Classes.Interfaces;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using NUnit.Framework;
+using NSubstitute;
+using UsbSimulator;
+
+namespace Handin.Test.Unit
+{
+    public class StationControl_UTest
+    {
+        private StationControl _station;
+        private IRFIDReader _rfidReader;
+        private IDisplay _display;
+        private IChargeControl _chargeControl;
+        private ILog _log;
+        private IDoor _door;
+        private IUsbCharger _charger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rfidReader = Substitute.For<IRFIDReader>();
+            _display = Substitute.For<IDisplay>();
+            _charger = Substitute.For<IUsbCharger>();
+            _chargeControl = Substitute.For<IChargeControl>();
+            _log = Substitute.For<ILog>();
+            _door = Substitute.For<IDoor>();
+
+            _station = new StationControl(_door, _chargeControl, _rfidReader, _display, _log);
+        }
+
+
+
+        [Test]
+        public void Test_DisplaysWhenPhoneIsPluggedIn()
+        {
+            _chargeControl.IsConnected().Returns(true);
+            _door.DoorCloseEvent += Raise.EventWith(null, new DoorEventArgs());
+            _rfidReader.RFIDReadEvent += Raise.EventWith(null, new RFIDEventArgs() {id = 3});
+            _display.Received(3);
+        }
+
+        [Test]
+        public void Test_DisplaysWhenRFIDEventIsCalled()
+        {
+            _door.DoorCloseEvent += Raise.EventWith(null, new DoorEventArgs());
+            _rfidReader.RFIDReadEvent += Raise.EventWith(null, new RFIDEventArgs() {id = 3});
+            _display.Received(1).DisplayMsg(MessageType.ConnectionError);
+        }
+    }
+}
